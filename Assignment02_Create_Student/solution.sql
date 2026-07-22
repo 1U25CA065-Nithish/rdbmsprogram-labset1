8330c7408c0ba296b9c9355bb65b32f302926ebe
1U25CA065-Nithish <maninithish56@gmail.com>
2026-07-22 16:07:11 +0530

    Create CollegeDB and Student table
    Added SQL commands to create the CollegeDB database and the Student table with appropriate fields and constraints.

--- Assignment02_Create_Student/solution.sql
+++ Assignment02_Create_Student/solution.sql
@@ -15,3 +15,13 @@ USE CollegeDB;
 -- DepartmentID
 
 -- Add constraints
+
+USE CollegeDB;
+
+CREATE TABLE Student (
+    StudentID INT PRIMARY KEY,
+    StudentName VARCHAR(20) NOT NULL,
+    DOB DATE NOT NULL,
+    Gender VARCHAR(10) NOT NULL,
+    DepartmentID INT NOT NULL
+);
